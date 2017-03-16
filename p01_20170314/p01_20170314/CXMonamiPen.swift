1e8822e2ce08c6b113d0b211b62eeb97a63a16ec
jjaygim <jjaygim@gmail.com>
2017-03-16 20:36:33 +0900

    P1, P1-2 마무리

--- p01_20170314/p01_20170314/CXMonamiPen.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CXMonamiPen.swift
-//  p01_20170314
-//
-//  Created by 김재정 on 2017. 3. 15..
-//  Copyright © 2017년 jjaygim. All rights reserved.
-//
-
-import Foundation
--- p01_20170314/p01_20170314/FileMatcher.swift
+++
@@ -1,9 +0,0 @@
-//
-//  FileMatcher.swift
-//  p01_20170314
-//
-//  Created by 김재정 on 2017. 3. 14..
-//  Copyright © 2017년 jjaygim. All rights reserved.
-//
-
-import Foundation
