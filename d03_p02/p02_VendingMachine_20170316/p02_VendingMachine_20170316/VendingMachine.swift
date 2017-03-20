e035862eabac782d40c126a5df783a9e23e49e7f
jjaygim <jjaygim@gmail.com>
2017-03-21 00:19:11 +0900

    VendingMachine(Beverage Class Only)

---
+++ d03_p02/p02_VendingMachine_20170316/p02_VendingMachine_20170316/VendingMachine.swift
@@ -0,0 +1,69 @@
+//
+//  VendingMachine.swift
+//  p02_VendingMachine_20170316
+//
+//  Created by 김재정 on 2017. 3. 18..
+//  Copyright © 2017년 jjaygim. All rights reserved.
+//
+
+import Foundation
+
+class VendingMachine {
+    private var stocks: [Beverage]
+    private var cash: Int
+    private var stocksDict: [String:Int]
+    
+    init() {
+        stocks = [Beverage]()
+        cash = Int()
+        stocksDict = [String:Int]()
+    }
+    
+    func add(_ beverage: Beverage) {
+        stocksDict[beverage.getBrand()] = beverage.getPrice()
+    }
+    
+    func getNamesOfStocks() -> [String] {
+        var tempSet = Set<String>()
+        for item in stocks {
+            tempSet.insert(item.getBrand())
+        }
+        return Array(tempSet)
+    }
+    
+    func getBeveragePrice(name: String) -> Int {
+        let filtered = stocks.filter { $0.getBrand() == name }
+        return filtered[0].getPrice()
+    }
+    
+    func getAllStocks() -> [String:Int] {
+        var tempDict = [String:Int]()
+        for item in getNamesOfStocks() {
+            let filtered:[Beverage] = stocks.filter { $0.getBrand() == item }
+            tempDict[filtered[0].getBrand()] = filtered.count
+        }
+        stocksDict = tempDict
+        return tempDict
+    }
+    
+    func getAvailable(price: Int) -> [String] {
+        var tempArray = [String]()
+        for item in getNamesOfStocks() {
+            if getBeveragePrice(name: item) <= price {
+                tempArray.append(item)
+            }
+        }
+        return tempArray
+    }
+    
+    func buy(name: String, inputPrice: Int, amount: Int) {
+        if getAvailable(price: inputPrice).contains(name) != false {
+            return
+        }
+        if getBeveragePrice(name: name) * amount > inputPrice {
+            return
+        }
+    }
+}
+    
+
---
+++ d03_p02/p02_VendingMachine_20170316/p02_VendingMachine_20170316/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  p02_VendingMachine_20170316
+//
+//  Created by 김재정 on 2017. 3. 17..
+//  Copyright © 2017년 jjaygim. All rights reserved.
+//
+
+import Foundation
+
